d48e4a1d0ee3e1b120f18c43d0b5e44c40bf6387
amchisa <chisa.alexander@gmail.com>
2025-07-15 18:50:25 -0400

    Set up project structure

---
+++ backend/src/main/java/com/amchisa/lucidflow/LucidFlowApplication.java
@@ -0,0 +1,13 @@
+package com.amchisa.lucidflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucidFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucidFlowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/amchisa/lucidflow/LucidFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amchisa.lucidflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucidFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
