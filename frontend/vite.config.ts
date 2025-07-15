8d42c112f302be5fd05288b5e4690b088461d5f0
amchisa <chisa.alexander@gmail.com>
2025-07-15 19:18:57 -0400

    Added and removed dependencies and cleared pregenerated vite output

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: './',
   build: {
     outDir: '../backend/src/main/resources/static',
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
