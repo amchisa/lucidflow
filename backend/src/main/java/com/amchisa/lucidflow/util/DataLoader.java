b24357b6e0724f62b6a8be1ff20d638a23290d30
amchisa <chisa.alexander@gmail.com>
2025-09-08 19:14:51 -0400

    Add dataloader customizable environment variables

--- backend/src/main/java/com/amchisa/lucidflow/util/DataLoader.java
+++ backend/src/main/java/com/amchisa/lucidflow/util/DataLoader.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,26 +16,43 @@ import java.util.List;
 
 @Component
 public class DataLoader implements CommandLineRunner {
-    private static final String DATA_FILE_PATH = "/sample-data.json"; // Change if needed
     private static final Logger logger = LoggerFactory.getLogger(DataLoader.class);
 
     private final PostService postService;
     private final ObjectMapper objectMapper;
+    private final String dataFilePath;
+    private final boolean enabled;
 
-    public DataLoader(PostService postService, ObjectMapper objectMapper) {
+    public DataLoader(
+        PostService postService,
+        ObjectMapper objectMapper,
+        @Value("${dataloader.datafile-path}") String dataFilePath,
+        @Value("${dataloader.enabled:true}") boolean enabled
+    ) {
         this.postService = postService;
         this.objectMapper = objectMapper;
+        this.dataFilePath = dataFilePath;
+        this.enabled = enabled;
     }
 
     @Override
     public void run(String... args) {
-        if (postService.postCount() > 0) { // Avoid duplicating db entries
-            logger.info("Database already populated. Skipping data load.");
+        if (!enabled) {
             return;
         }
 
+        if (dataFilePath == null || dataFilePath.isBlank()) {
+            logger.warn("Data file path not provided. Skipping data load.");
+            return;
+        }
+
+        if (postService.postCount() > 0) {
+            logger.info("Database already populated. Skipping data load.");
+            return; // Avoid duplicating db entries
+        }
+
         try {
-            postService.createPosts(loadDataFromFile(DATA_FILE_PATH));
+            postService.createPosts(loadDataFromFile(dataFilePath));
             logger.info("Data load completed successfully.");
         }
         catch (IOException e) {
